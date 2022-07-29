2e16e28e1ece1db40757abfe79d1ccf92bdebc72
Annette Theodore <annettetheodore582@gmail.com>
2022-07-29 01:40:01 -1200

    feat: :sparkles: created Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className='Hero'>
+        <h2>Time,dedication,<br />and darn good eats.</h2>
+
+        <div className='text'>
+        <p>If breakfast is the most important meal of the day, then brunch is the most important 
+    meal of the week. We treat it with the pomp and ceremony it deserves, using farm-
+       fresh ingredients to craft food you’ll spend the next six days dreaming about.
+
+</p>
+</div>
+<div className='button'>
+    <button>OUR STORY</button>
+</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
