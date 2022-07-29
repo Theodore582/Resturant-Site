1d1e9d4a7048a7a86b57321e5ca6445e3efb91f1
Annette Theodore <annettetheodore582@gmail.com>
2022-07-29 01:43:28 -1200

    feat: :sparkles: imported images

---
+++ src/components/Gallery/images.js
@@ -0,0 +1,10 @@
+export { default as pic1 } from '../../Images/pic1.png'
+export { default as pic2 } from '../../Images/pic2.png'
+export { default as pic3 } from '../../Images/pic3.png'
+export { default as pic4 } from '../../Images/pic4.png'
+export { default as pic5 } from '../../Images/pic5.png'
+export { default as pic6 } from '../../Images/pic6.png'
+export { default as pic7 } from '../../Images/pic7.png'
+export { default as pic8 } from '../../Images/pic8.png'
+export { default as pic9 } from '../../Images/pic9.png'
+
