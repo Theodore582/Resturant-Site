d055c2da6a40125dd89d54f544467ad8393f8449
Annette Theodore <annettetheodore582@gmail.com>
2022-08-21 17:16:31 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
-import Hero from './components/Hero/Hero';
-import Home from './components/Home/Home';
-import Menu from './components/Menu/Menu';
-import FullMenu from './Full-Menu Section/FullMenu';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Gallery from "./components/Gallery/Gallery";
+import Hero from "./components/Hero/Hero";
+import Home from "./components/Home/Home";
+import Menu from "./components/Menu/Menu";
+import FullMenu from "./Full-Menu Section/FullMenu";
 
 function App() {
   return (
-   <div>
-   
-   <Home />
-   <Hero />
-  <Gallery />
-  <Menu />
-  <FullMenu />
-  <Footer />
-
-
-
-</div>
-    
- );
+    <div>
+      <Home />
+      <Hero />
+      <Gallery />
+      <Menu />
+      <FullMenu />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
